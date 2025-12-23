a0720df854c38bdd2030f6e1d66f7b1773bcbb4c
Qrawroar <250775154+Qrawroar@users.noreply.github.com>
2025-12-23 15:08:05 +0800

    Create Primitive.java

---
+++ Primitive.java
@@ -0,0 +1,11 @@
+class Main{
+  public static void main(String[] agrs){
+    int age = 16;
+    int year = 2024;
+    double price = 1.99;
+    char currency = '$';
+    String name = "Johny";
+    
+    
+  }
+}
