8886c2fe25f21d00692161dbec8c45cf51dab30d
Qrawroar <250775154+Qrawroar@users.noreply.github.com>
2025-12-23 15:14:21 +0800

    Update Primitive.java

--- Primitive.java
+++ Primitive.java
@@ -1,11 +1,16 @@
 class Main{
   public static void main(String[] agrs){
     int age = 16;
-    int year = 2024;
     double price = 1.99;
     char currency = '$';
     String name = "Johny";
     boolean isStudent = true;
+
+    System.out.println("My name is " + name);
+    System.out.println("I sell candies for " + price);
+    System.out.println("My currency is " + currency);
+    System.out.println("Im " + age + " years old");
+    System.out.println("Im student = " + isStudent);
     
   }
 }
